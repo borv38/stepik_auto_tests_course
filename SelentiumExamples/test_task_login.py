8cda5093fc18749ec08fd652bec47e754bbce745
vvborsce <borv38@gmail.com>
2023-10-20 10:35:11 +0300

    added authorization

--- SelentiumExamples/test_task_login.py
+++ SelentiumExamples/test_task_login.py
@@ -20,7 +20,7 @@ class TestMainPage1():
     def test_guest_should_see_login_link(self, browser):
         browser.get(link)
         time.sleep(15)
-        button = browser.find_element(By.CLASS_NAME, "ember-view navbar__auth navbar__auth_login st-link st-link_style_button")
+        button = browser.find_element(By.CSS_SELECTOR, "#ember36")
         button.click()
 
         input1 = browser.switch_to.alert
