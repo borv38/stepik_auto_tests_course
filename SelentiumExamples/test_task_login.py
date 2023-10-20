a47908f42774b5af55b5390eb32d2572274b4702
vvborsce <borv38@gmail.com>
2023-10-20 11:37:20 +0300

    added authorization

--- SelentiumExamples/test_task_login.py
+++ SelentiumExamples/test_task_login.py
@@ -4,17 +4,6 @@ from selenium.webdriver.common.by import By
 import time
 
 link = "https://stepik.org/lesson/236895/step/1"
-
-
-@pytest.fixture(scope="function")
-def browser():
-    print("\nstart browser for test..")
-    browser = webdriver.Chrome()
-    yield browser
-    print("\nquit browser..")
-    browser.quit()
-
-
 class TestMainPage1():
 
     def test_guest_should_see_login_link(self, browser):
